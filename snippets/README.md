166c4e5eb94f9fc686f5e879128fa09246274420
Peter PayteR Gašparík <payter86@gmail.com>
2019-03-29 06:21:53 +0100

    reworked to wor directly with bulma scss

--- snippets/README.md
+++ snippets/README.md
@@ -1,8 +1,10 @@
 # How to use this snippets
 
-Copy folder inside your scss folder and include styles 
+Copy folder inside your scss folder and include styles.
 
 ```
 /** Import cypher-bulma-layout **/
-@import "cypher-bulma-layout/_all";
+@import "bulma/_all";
 ```
+
+USE THIS INSTEAD OF INCLUDE BULMA SCSS!
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,12 @@
 const mix = require('laravel-mix');
 
-mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
-  .sass('scss/cypher-bulma-extensions.scss', 'public/dist')
+mix.sass('bulma_scss/_all.scss', 'public/dist')
   .browserSync({
     proxy: 'http://127.0.0.1:19011',
     files: ["public/**/*"],
     port: 19013,
+    ui: {
+      port: 19014
+    },
     watch: true
   });
